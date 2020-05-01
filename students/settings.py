aa182c48cbf30f1e47d7d7010c5cc6893dc7c261
exxonlin <62740802+exxonlin@users.noreply.github.com>
2020-05-01 14:52:26 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -35,9 +35,9 @@ from linebot.models import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN ='gCf0ZyEu16LMKoeXa94Q/OiUtoCA8IvES5yVgXOZ6R4mBJ2vN6RNCzY7hCbBlVv42d87f8AjuS9TjCu7R3nssQSN7gUFERzXThyn2g9nHTypLt6pvSnqtZ7aOBd1T9FNd03VkJgahBmoOqkJgoLYzgdB04t89/1O/w1cDnyilFU='
 # 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_SECRET='fb0ffe516ad0c66269cd4b1fd33140e0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
